71f71e1ef072dc337bb9d9609351c47feafcec7c
Anastasia Dmitrieva <fefu.dmitrieva@feip.co>
2024-11-12 16:49:21 +1000

    laba 1

---
+++ laba1/build.cmd
@@ -0,0 +1,10 @@
+git pull origin main
+
+mkdir build
+cd build
+
+cmake ../hello_world
+
+cmake --build .
+
+hello_world.exe
---
+++ laba1/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull origin main
+
+mkdir build
+cd build
+
+cmake ../hello_world
+
+cmake --build .
+
+./hello_world
