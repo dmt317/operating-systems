2826b947e4019e7ff15ebeda4a8b9e2ab5a1b7d8
dmt317 <anastasia2320281@gmail.com>
2025-01-11 18:29:18 +1000

    fix windows version

--- laba4/temperature_logger/include/serialport_reader.h
+++ laba4/temperature_logger/include/serialport_reader.h
@@ -3,9 +3,9 @@
 #define SERIALPORT_READER_H
 
 #ifdef _WIN32
-    HANDLE init_port_win(void);
+    HANDLE init_port_windows(void);
 
-    float read_from_port_posix(HANDLE hComm);
+    float read_from_port_windows(HANDLE hComm);
 #else
     int init_port_posix(void);
 
