2dc6bbbd0842bde1212fde741b71700f1d791f1f
dmt317 <anastasia2320281@gmail.com>
2025-01-17 13:21:13 +1000

    f

--- laba4/temperature_logger/src/platform/posix/serialport_reader_posix.c
+++ laba4/temperature_logger/src/platform/posix/serialport_reader_posix.c
@@ -7,7 +7,7 @@
 
 #include "../../../include/serialport_reader.h"
 
-#define PORT_NAME "/dev/ttys003"
+#define PORT_NAME "/dev/ttys007"
 
 int init_port_posix() {
     int fd = open(PORT_NAME, O_RDONLY | O_NOCTTY);
